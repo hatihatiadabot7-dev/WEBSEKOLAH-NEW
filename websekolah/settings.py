6d978e9c7c48eca721eaab3c6b31ac13e7c3b4a1
hatihatiadabot7-dev <hatihatiadabot7@gmail.com>
2026-06-13 20:41:35 +0700

    update

--- websekolah/settings.py
+++ websekolah/settings.py
@@ -23,9 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7cqseum1mghp)7g5fs-#fs0m&aas6#$q%d5v9snpk4urr5fr0q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+DEBUG = False
+ALLOWED_HOSTS = ["aisyarkaanabdurrahman.pythonanywhere.com"]
 
 
 # Application definition
--- websekolah/wsgi.py
+++ websekolah/wsgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
+# Mengarahkan ke folder utama kamu di PythonAnywhere
+path = '/home/aisyarkaanabdurrahman/WEBSEKOLAH-NEW'
+if path not in sys.path:
+    sys.path.append(path)
 
+# Mengarahkan ke nama folder project django kamu (websekolah)
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'websekolah.settings')
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
